6a086570a557e58174a7911bad81f1a2f4f3ba2a
Aarón Martín <aamarber@gmail.com>
2023-10-31 09:39:36 +0100

    Possible tailwind solution

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html'
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      "a": "#7F534B",
+      "b": "#1F0318",
+      "c": "#1E1A1D",
+      "d": "#E5F2C9",
+      "e": "#8C705F"
+    }
+  },
+  plugins: [],
+}
+
